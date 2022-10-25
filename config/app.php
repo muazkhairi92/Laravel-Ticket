645ea5a41a745e20427d7b69e292d6ece4a4da6c
muazkhairi92 <muazkhairi@alumni.iastate.edu>
2022-10-25 10:37:43 +0800

    edit url

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://solverticket2.vercel.app/her'),
 
     'asset_url' => env('ASSET_URL', null),
 
